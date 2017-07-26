ac718b6a36aa5e687aae4b0e150eef13b2206698
DESKTOP-IFAEVE3\Ethan <embrown801@gmail.com>
2017-07-26 12:23:46 -0600

    Initial Commit. ItemWindow GUI and sql class.

---
+++ 3280_GroupAssignment/GroupAssignment/ItemsWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Reflection;
+
+namespace GroupAssignment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ItemsWindow : Window
+    {
+        /// <summary>
+        /// creates access to the clsSQL class
+        /// </summary>
+        clsSQL df = new clsSQL();
+
+        public ItemsWindow()
+        {
+            try
+            { 
+                InitializeComponent();
+                //Bind the DataSet to the DataGridView
+                itemDesc.ItemsSource = df.getData();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// handles any errors that come up
+        /// </summary>
+        /// <param name="sClass"></param>
+        /// <param name="sMethod"></param>
+        /// <param name="sMessage"></param>
+        private void HandleError(string sClass, string sMethod, string sMessage)
+        {
+            try
+            {
+                MessageBox.Show(sClass + "." + sMethod + " -> " + sMessage);
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.AppendAllText("C:\\Error.txt", Environment.NewLine +
+                                            "HandleError Exception: " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Adds a new item to the itemDesc table in the Database
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void addItem_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+              
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Edits the selected item in the data grid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void editItemBTN_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Delete Selected Item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void deleteItemBTN_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// takes the user back to the main menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mainMenuBTN_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+    }
+}
---
+++ 3280_GroupAssignment/GroupAssignment/clsSQL.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace GroupAssignment
+{
+    class clsSQL : INotifyPropertyChanged
+    {
+        DataSet ds = new DataSet();
+
+        clsDataAccess db = new clsDataAccess();
+
+        public DataView getData()
+        {
+            try
+            {
+                //Number of return values
+                int iRet = 0;
+                //Get the data to be put into the DataGridView
+                ds = db.ExecuteSQLStatement("SELECT * FROM ItemDesc ORDER BY ItemCode", ref iRet);
+                //Set the table name in the DataSet
+                ds.Tables[0].TableName = "Item Table";
+
+                return ds.Tables[0].DefaultView;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// addes a new row with passed in values into the Database. 
+        /// </summary>
+        /// <param name="itemDesc"></param>
+        /// <param name="cost"></param>
+        public void addNewItem(string itemCode, string itemDesc, string cost)
+        {
+            try
+            {
+                string sSQL = "INSERT INTO ItemDesc (ItemCode, ItemDesc, Cost) VALUES ('" + itemCode + "', '" + itemDesc + "', " + cost + ")";
+                db.ExecuteNonQuery(sSQL);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// updates the line that the item code is a part of with the passed in values
+        /// </summary>
+        /// <param name="itemCode"></param>
+        /// <param name="itemDesc"></param>
+        /// <param name="cost"></param>
+        public void updateItem(string itemCode, string itemDesc, string cost)
+        {
+            try
+            {
+                string sSQL = "UPDATE ItemDesc SET ItemDesc = '" + itemDesc + "', Cost = '" + cost + "' WHERE ItemCode = '" + itemCode + "'";
+                db.ExecuteNonQuery(sSQL);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Deletes the row that the passed in item code is a part of
+        /// </summary>
+        /// <param name="itemCode"></param>
+        public void deleteItem(string itemCode)
+        {
+            try
+            {
+                string sSQL = "DELETE FROM ItemDesc WHERE ItemCode = '" + itemCode + "'";
+                db.ExecuteNonQuery(sSQL);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
